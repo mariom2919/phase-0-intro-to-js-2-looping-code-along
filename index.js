a7ebd1b5c7a6918c715fbf6a27cec3de7a964613
+ <+>
2024-06-21 02:08:16 -0700

    -m added for loop

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+function countDown() {
+    for (let i = 10; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(); // Call the function to start the countdown
+  
